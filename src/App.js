fec2b6f2a220fab4aa20af9940228f66d63d6b47
OMaxime68 <maxime.owaller@epitech.eu>
2023-09-18 17:04:56 +0200

    Add Sidebar & Home components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import About from './components/about/About'
-import Blog from './components/blog/Blog'
-import Contact from './components/contact/Contact'
+import Sidebar from './components/sidebar/Sidebar'
 import Home from './components/home/Home'
+import About from './components/about/About'
+import Services from './components/services/Services'
+import Resume from './components/resume/Resume'
 import Portfolio from './components/portfolio/Portfolio'
 import Pricing from './components/pricing/Pricing'
-import Resume from './components/resume/Resume'
-import Services from './components/services/Services'
-import Sidebar from './components/sidebar/Sidebar'
 import Testimonials from './components/testimonials/Testimonials'
+import Blog from './components/blog/Blog'
+import Contact from './components/contact/Contact'
 
 function App() {
   return (
---
+++ src/components/home/HeaderSocials.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className="home__socials">
+        <a href="" className="home__social-link" target='_blank'>
+        <i class="fa-brands fa-youtube"></i>
+        </a>
+
+        <a href="" className="home__social-link" target='_blank'>
+        <i class="fa-brands fa-twitter"></i>
+        </a>
+
+        <a href="" className="home__social-link" target='_blank'>
+        <i class="fa-brands fa-behance"></i>
+        </a>
+
+        <a href="" className="home__social-link" target='_blank'>
+        <i class="fa-brands fa-dribble"></i>
+        </a>
+    </div>
+  )
+}
+
+export default HeaderSocials
