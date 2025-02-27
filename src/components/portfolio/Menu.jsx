e4f75792ecea9cbd65fabdb035e9b24f993a9dc3
OMaxime68 <maxime.owaller@epitech.eu>
2025-02-27 19:32:06 +0100

    add project

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -1,53 +1,61 @@
-import Portfolio from "../../assets/works/portfolio.jpg"
-import CarRacing from "../../assets/works/car-racing.jpg"
-import Matchstick from "../../assets/works/matchstick.jpg"
-import Maze from "../../assets/works/maze.jpg"
-import Navy from "../../assets/works/navy.jpg"
-import RPG from "../../assets/works/rpg.jpg"
+import Portfolio from "../../assets/works/portfolio.jpg";
+import CarRacing from "../../assets/works/car-racing.jpg";
+import Matchstick from "../../assets/works/matchstick.jpg";
+import Maze from "../../assets/works/maze.jpg";
+import Navy from "../../assets/works/navy.jpg";
+import RPG from "../../assets/works/rpg.jpg";
+import MeilleurMood from "../../assets/works/meilleurmood.jpg";
 
 const Menu = [
-    {
-      id: 1,
-      name: "portfolio",
-      image: Portfolio,
-      category: "web",
-      link: 'https://github.com/OMaxime68/portfolio',
-    },
-    {
-      id: 2,
-      name: "car_racing",
-      image: CarRacing,
-      category: "ai",
-      link: 'https://github.com/nathan-hoche/CarRacing',
-    },
-    {
-      id: 3,
-      name: "matchstick",
-      image: Matchstick,
-      category: "software",
-      link: 'https://github.com/OMaxime68/CPE_matchstick_2019',
-    },
-    {
-      id: 4,
-      name: "dante",
-      image: Maze,
-      category: "software",
-      link: 'https://github.com/OMaxime68/CPE_dante_2019',
-    },
-    {
-      id: 5,
-      name: "navy",
-      image: Navy,
-      category: "software",
-      link: 'https://github.com/OMaxime68/PSU_navy_2019',
-    },
-    {
-      id: 6,
-      name: "rpg",
-      image: RPG,
-      category: "software",
-      link: 'https://github.com/OMaxime68/MUL_my_rpg_2019',
-    },
+  {
+    id: 1,
+    name: "meilleurmood",
+    image: MeilleurMood,
+    category: "web",
+    link: "https://meilleurmood.fr",
+  },
+  {
+    id: 2,
+    name: "portfolio",
+    image: Portfolio,
+    category: "web",
+    link: "https://github.com/OMaxime68/portfolio",
+  },
+  {
+    id: 3,
+    name: "car_racing",
+    image: CarRacing,
+    category: "ai",
+    link: "https://github.com/nathan-hoche/CarRacing",
+  },
+  {
+    id: 4,
+    name: "matchstick",
+    image: Matchstick,
+    category: "software",
+    link: "https://github.com/OMaxime68/CPE_matchstick_2019",
+  },
+  {
+    id: 5,
+    name: "dante",
+    image: Maze,
+    category: "software",
+    link: "https://github.com/OMaxime68/CPE_dante_2019",
+  },
+  {
+    id: 6,
+    name: "navy",
+    image: Navy,
+    category: "software",
+    link: "https://github.com/OMaxime68/PSU_navy_2019",
+  },
+  {
+    id: 7,
+    name: "rpg",
+    image: RPG,
+    category: "software",
+    link: "https://github.com/OMaxime68/MUL_my_rpg_2019",
+  },
 ];
 
 export default Menu;
